3f3b120de3ba03b941302eb5c7e074e2c02fe2f6
Nik <nik.ignition.work@gmail.com>
2020-06-04 14:55:35 +0300

    binded photos to view

--- app/src/main/java/com/goncharoff/testapp/MainActivity.java
+++ app/src/main/java/com/goncharoff/testapp/MainActivity.java
@@ -1,14 +1,16 @@
 package com.goncharoff.testapp;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.goncharoff.testapp.adapter.PhotosAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +26,19 @@ public class MainActivity extends AppCompatActivity {
     private TextView subsNumber;
     private TextView rating;
 
+    private RecyclerView photosRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initProfileViews();
-        initProfileViewsContent();
+        initViews();
+        initViewsContent();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     private ProfileViewModel getProfileViewModel() {
@@ -40,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void initProfileViews() {
+    private void initViews() {
         avatar = findViewById(R.id.avatar);
         name = findViewById(R.id.name);
         professionText = findViewById(R.id.professionText);
@@ -50,9 +59,10 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.emailAddress);
         subsNumber = findViewById(R.id.subsNumber);
         rating = findViewById(R.id.rating);
+        photosRecyclerView = findViewById(R.id.photosGridView);
     }
 
-    private void initProfileViewsContent() {
+    private void initViewsContent() {
         Glide.with(this).load(getProfileViewModel().getUserData().getPictureUrl()).into(avatar);
         name.setText(getString(R.string.name, getProfileViewModel().getUserData().getName(), getProfileViewModel().getUserData().getLastName()));
         professionText.setText(getProfileViewModel().getUserData().getTitle());
@@ -62,5 +72,10 @@ public class MainActivity extends AppCompatActivity {
         email.setText(getProfileViewModel().getUserData().getEmail());
         subsNumber.setText(getProfileViewModel().getUserData().getFollowers());
         rating.setText(String.valueOf(getProfileViewModel().getUserData().getRating()));
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        photosRecyclerView.setLayoutManager(layoutManager);
+        photosRecyclerView.setAdapter(new PhotosAdapter(this, getProfileViewModel().getPostData().getPhotos()));
     }
 }
--- app/src/main/java/com/goncharoff/testapp/ProfileViewModel.java
+++ app/src/main/java/com/goncharoff/testapp/ProfileViewModel.java
@@ -1,21 +1,26 @@
 package com.goncharoff.testapp;
 
-import android.util.Log;
-
 import androidx.lifecycle.ViewModel;
 
 import com.goncharoff.testapp.domain.User;
+import com.goncharoff.testapp.domain.post.PostData;
 import com.goncharoff.testapp.repository.UserRepository;
 
 public class ProfileViewModel extends ViewModel {
 
     private User userData;
+    private PostData postData;
 
     public ProfileViewModel() {
         userData = UserRepository.getINSTANCE().getUserData();
+        postData = UserRepository.getINSTANCE().getPostData();
     }
 
     public User getUserData() {
         return userData;
+    }
+
+    public PostData getPostData() {
+        return postData;
     }
 }
